61619fa8b8f07be249fb32b5adce4fd177d66d42
makyfj <65879341+makyfj@users.noreply.github.com>
2021-10-10 16:12:23 -0400

    chore: added body-date-title of each article

--- src/main.py
+++ src/main.py
@@ -6,13 +6,26 @@ adelphiNewsPage = requests.get("https://www.adelphi.edu/news")
 contents = adelphiNewsPage.content
 
 soup = BeautifulSoup(contents, "html.parser")
-# print(soup.prettify())
-
-pageTitle = soup.title
 
 # retrieves the title of each article in the front page
 newsTitle = soup.find_all("span", "news_item_title_link_label")
 
+# publication date of the article
+newsPublicationDate = soup.find_all("time", "news_item_detail_label")
+
+# body of the article
+newsBody = soup.find_all("div", "news_item_description")
+
+# number of articles in the front page
+articles = len(newsTitle)
+print(articles)
+
+# for body in newsBody:
+#    print(body.p.string)
+
+# for date in newsPublicationDate:
+#     print(date.string)
+
 # prints the title of the article
-for title in newsTitle:
-    print(title.string)
+# for title in newsTitle:
+#     print(title.string)
