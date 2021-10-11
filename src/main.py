45628353aee0f0573ca3a180953a5463310ea4b9
makyfj <65879341+makyfj@users.noreply.github.com>
2021-10-10 20:36:31 -0400

    chore: storing all articles in a 2d array

--- src/main.py
+++ src/main.py
@@ -10,22 +10,34 @@ soup = BeautifulSoup(contents, "html.parser")
 # retrieves the title of each article in the front page
 newsTitle = soup.find_all("span", "news_item_title_link_label")
 
+# number of articles
+numberOfNews = len(newsTitle)
+
 # publication date of the article
 newsPublicationDate = soup.find_all("time", "news_item_detail_label")
 
 # body of the article
 newsBody = soup.find_all("div", "news_item_description")
 
-# number of articles in the front page
-articles = len(newsTitle)
-print(articles)
+titles = []
+dates = []
+bodies = []
 
-# for body in newsBody:
-#    print(body.p.string)
+for title in newsTitle:
+    titles.append(title.string)
 
-# for date in newsPublicationDate:
-#     print(date.string)
+for date in newsPublicationDate:
+    dates.append(date.string)
 
-# prints the title of the article
-# for title in newsTitle:
-#     print(title.string)
+for body in newsBody:
+    bodies.append(body.p.string)
+
+# To store all articles, to access them easily
+articles = []
+
+for i in range(numberOfNews):
+     # articles[i].append({"title": titles[i], "date": dates[i], "body": bodies[i]})
+    articles.append([titles[i], bodies[i], dates[i]])
+
+# First article
+print(articles[0])
