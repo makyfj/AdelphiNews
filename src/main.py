09ba77d7336b8d6443d207b233b6d297907e0409
Maky <franklinjaradev@gmail.com>
2021-10-12 23:33:56 -0400

    chore: added category for each article

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ adelphiNewsPage = requests.get("https://www.adelphi.edu/news")
 
 contents = adelphiNewsPage.content
 
+
 soup = BeautifulSoup(contents, "html.parser")
 
 # retrieves the title of each article in the front page
@@ -20,9 +21,14 @@ newsPublicationDate = soup.find_all("time", "news_item_detail_label")
 # body of the article
 newsBody = soup.find_all("div", "news_item_description")
 
+# category of the article
+newsCategory = soup.find_all("li", "news_item_category" )
+
+
 titles = []
 dates = []
 bodies = []
+categories = []
 
 for title in newsTitle:
     titles.append(title.string)
@@ -33,16 +39,23 @@ for date in newsPublicationDate:
 for body in newsBody:
     bodies.append(body.p.string)
 
+
+for category in newsCategory:
+    # for ul - news_item_category_list
+    # categories.append({"category": [", ".join((category.text).strip("\n").split("\n\n\n"))]})
+    categories.append({"category": category.text.strip("\n")})
+
+
 # To store all articles, to access them easily
 articles = [] 
 
 for i in range(numberOfNews):
-    articles.append({"title": titles[i], "date": dates[i], "body": bodies[i]})
+     articles.append({"title": titles[i],"category": categories[i] ,"date": dates[i], "body": bodies[i]})
 
 # How to access the articles
-print(articles[0]["title"])
-print(articles[0]["date"])
-print(articles[0]["body"])
+# print(articles[0]["title"])
+# print(articles[0]["date"])
+# print(articles[0]["body"])
 
 
 with open("articles.json", 'w') as outfile:
